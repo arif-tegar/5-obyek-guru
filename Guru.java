909d2b658d7a4885f6bc60a42caa3de1b470e98a
arif-tegar <arif_karim_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:56:44 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,19 @@
+//Object Class
+public class Guru{
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    //construktor Guru 
+    public void guru () {
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+
+    }
+
+}
+    
+
